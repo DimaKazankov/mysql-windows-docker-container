4f7b87c763c459b574188d343b926d4eac6a74ba
Dmitry Kazankov <Dima.Kazankov@gmail.com>
2018-11-28 21:27:07 +0100

    Update initmysql.cmd

--- initmysql.cmd
+++ initmysql.cmd
@@ -1,7 +1,9 @@
-setx PATH '%PATH%;C:\MySQL\bin'
-C:\MySQL\bin\mysqld --initialize-insecure --console --explicit_defaults_for_timestamp
-C:\MySQL\bin\mysqld --install MySQL --defaults-file="C:\MySQL\my.ini"
+setx PATH '%PATH%;C:\app\MySQL\bin'
+C:\app\MySQL\bin\mysqld --initialize-insecure --console --explicit_defaults_for_timestamp
+C:\app\MySQL\bin\mysqld --install MySQL --defaults-file="C:\app\MySQL\my.ini"
 
 powershell Start-Service mysql
+
+C:\app\MySQL\bin\mysql -e "CREATE DATABASE rocs_dev; quit;"
 
 ping localhost -t
